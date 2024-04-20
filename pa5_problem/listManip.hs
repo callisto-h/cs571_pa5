020aa351e1368d373ef8bc46c0e654410c0fb1b0
Callisto Hess <callisto@Callistos-MacBook-Pro.local>
2024-04-20 13:11:57 -0400

    ginished listmanip

--- pa5_problem/listManip.hs
+++ pa5_problem/listManip.hs
@@ -1,6 +1,10 @@
 
 prefix :: Eq a => [a] -> [a] -> Bool
-prefix = ???
+prefix [] _ = True
+prefix _ [] = False
+prefix (h1:t1) (h2:t2)
+    | h1 == h2 = prefix t1 t2
+    | otherwise = False
 
 -- Should output "True"
 test1 = 
@@ -17,9 +21,10 @@ test2 =
 
 
 sublist :: [a] -> [[a]]
-sublist lst = ???
-
-
+sublist []     = [[]]
+sublist (h:t) = let rest = sublist t
+                 in map (h:) rest ++ rest
+                 
 -- Should output "[[], [1], [2], [3], [1, 2], [2, 3], [1,3], [1, 2, 3]]"
 test3 =
     let input = [1,2,3] in
